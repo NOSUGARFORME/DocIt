b49b3265d74dc5b8ee401ba9939ae06102333db6
Ivan Bolsun <ivanbolsun03@gmail.com>
2023-06-26 23:47:55 +0300

    init

--- src/DocIt.Shared/WebApplication1/Controllers/WeatherForecastController.cs
+++ src/DocIt.Shared/WebApplication1/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace Task.Api.Controllers;
+namespace WebApplication1.Controllers;
 
 [ApiController]
 [Route("[controller]")]
--- src/DocIt.Shared/WebApplication1/WeatherForecast.cs
+++ src/DocIt.Shared/WebApplication1/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace Task.Api;
+namespace WebApplication1;
 
 public class WeatherForecast
 {
